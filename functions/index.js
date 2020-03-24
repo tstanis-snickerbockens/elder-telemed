aa174f6e1da9896de2d3794eb796053a5f8db0bb
Tom Stanis <tstanis@gmail.com>
2020-03-24 09:13:28 -0700

    almost got things connecting

--- functions/index.js
+++ functions/index.js
@@ -9,31 +9,38 @@ const cors = require('cors')({origin: true});
 const app = express();
 
 app.use(cors);
+const db = admin.database();
 
 exports.sendMessage = functions.https.onRequest((request, response) => {
-    const payload = {
-        notification: {
-            title: 'You have a new message!',
-            body: 'message body here',
-            icon: 'none'
-        }
-    };
-
     return cors(request, response, () => {
+        console.log("Saving: " + JSON.stringify(request.body.data, null, 2));
+        var requesterId = request.body.data.id;
+        var ref = db.ref("messages");
+        ref.transaction(function (current_value) {
+            if (current_value == null) {
+                current_value = {}
+            }
+            console.log("Write to " + requesterId);
+            current_value[requesterId] = request.body.data;
+            return current_value;
+        });
         response.status(200).send({'data': {'text':"Hello from Firebase!"}});
     });
 });
 
 exports.readMessage = functions.https.onRequest((request, response) => {
-    const payload = {
-        notification: {
-        title: 'You have a new message!',
-        body: 'message body here',
-        icon: 'none'
-        }
-    };
-
+    var ref = db.ref("messages");
     return cors(request, response, () => {
-        response.status(200).send({'data': {'text':"Hello from Firebase!"}});
+        var requesterId = request.body.data.id;
+        ref.once("value", function(data) {
+            console.log("Loaded: " + JSON.stringify(data, null, 2));
+            if (requesterId.toString() in data) {
+                console.log("Deleting own key");
+                delete data[requesterId.toString()]
+            }
+            console.log("RequesterId: " + requesterId);
+            console.log("Returning: " + JSON.stringify(data, null, 2));
+            response.status(200).send({'data': data});
+        })
     });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -39,11 +39,12 @@ document.getElementById('callButton').addEventListener('click', function() {
     pc.createOffer()
       .then(offer => pc.setLocalDescription(offer) )
       .then(() => sendRemoteMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
-    setInterval(readRemoteMessage, 1000);
+    setTimeout(readRemoteMessage, 1000);
   }
 });
 
 var yourId = Math.floor(Math.random()*1000000000);
+console.log("ID for this Client: " + yourId);
 var servers = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'webrtc','username': 'websitebeaver@mail.com'}]};
 var pc = new RTCPeerConnection(servers);
 pc.onicecandidate = (event => event.candidate?sendRemoteMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
@@ -51,7 +52,7 @@ pc.onaddstream = (event => remoteVideo.srcObject = event.stream);
 
 function sendRemoteMessage(senderId, data) {
   var sendMessage = firebase.functions().httpsCallable('sendMessage');
-  sendMessage({'id:': senderId, 'data': data}).then(function(result) {
+  sendMessage({'id': senderId, 'data': data}).then(function(result) {
       // Read result of the Cloud Function.
       console.log(result.data.text)
   });
@@ -60,20 +61,28 @@ function sendRemoteMessage(senderId, data) {
 function readRemoteMessage() {
   console.log("checking messages");
   var readMessage = firebase.functions().httpsCallable('readMessage');
-  readMessage({'id:': yourId}).then(function(data) {
-    var msg = JSON.parse(data.val().message);
-    var sender = data.val().sender;
-    if (sender != yourId) {
-      if (msg.ice != undefined) {
-        pc.addIceCandidate(new RTCIceCandidate(msg.ice));
-      } else if (msg.sdp.type == "offer") {
-        pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
-          .then(() => pc.createAnswer())
-          .then(answer => pc.setLocalDescription(answer))
-          .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
-      } else if (msg.sdp.type == "answer") {
-        pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+  readMessage({'id': yourId}).then(function(response) {
+    var data = response.data;
+    var found = false;
+    for (var sender in response.data) {
+      var msg = JSON.parse(response.data[sender].data);
+      if (sender != yourId) {
+        console.log(JSON.stringify(msg));
+        if (msg.ice != undefined) {
+          console.log('addIceCandidate');
+          pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+        } else if (msg.sdp.type == "offer") {
+          console.log('sdp offer')
+          pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+            .then(() => pc.createAnswer())
+            .then(answer => pc.setLocalDescription(answer))
+            .then(() => sendRemoteMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
+        } else if (msg.sdp.type == "answer") {
+          console.log('sdp answer')
+          pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+        }
       }
+      break;
     }
   });
 };
