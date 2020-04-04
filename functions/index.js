1c258c07718e00a1e4a3fee88f37d7b4866dfffd
Tom Stanis <tstanis@gmail.com>
2020-04-04 08:36:31 -0700

    annotateTranscription sends data to AWS comprehend medical

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,8 @@ const app = express();
 app.use(cors);
 let db = admin.firestore();
 
+var AWS = require("aws-sdk");
+
 // curl -X POST -H "Content-Type:application/json" http://localhost:5001/elder-telemed/us-central1/createEncounter -d '{"data": {"encounterId": "myencounter4", "encounter": {"patient":"mypatient"}}}'
 exports.createEncounter = functions.https.onRequest((request, response) => {
     return cors(request, response, () => {
@@ -257,86 +259,27 @@ exports.readMessage = functions.https.onRequest((request, response) => {
     });
 });
 
-
-exports.sendMessageFirestore = functions.https.onRequest((request, response) => {
+exports.annotateTranscription = functions.https.onRequest((request, response) => {
     return cors(request, response, () => {
-        console.log("Saving: " + JSON.stringify(request.body.data, null, 2));
-        var requesterId = request.body.data.id;
-        var encounterId = request.body.data.encounterId;
-        console.log("EncounterID: " + encounterId + " RequestionId: " + requesterId);
-        let ref = db.collection('encounters').doc(encounterId).collection('webrtc_signal_queue').doc(requesterId.toString());
-        let transaction = db.runTransaction(t => {
-            return t.get(ref)
-                .then(doc => {
-                    console.log("sendMessage success get");
-                    if (!doc.exists) {
-                        console.log("sendMessage no doc");
-                        data = {'queue':[request.body.data]};
-                        t.set(ref, data);
+        try {
+            AWS.config.loadFromPath("aws_credentials.json");
+            var message = request.body.data.message;
+            var params = {
+                'Text': message
+            };
+            var comprehendmedical = new AWS.ComprehendMedical();
+            comprehendmedical.detectEntitiesV2(params, 
+                function(err, data) {
+                    if (err) {
+                        response.status(500).send(err);
+                        console.log(err, err.stack);
                     } else {
-                        console.log("Current Value: " + JSON.stringify(doc.data(), null, 2));
-                        var data = doc.data()
-                        data['queue'].push(request.body.data);
-                        t.update(ref, data);
+                        response.status(200).send({'data':data});
+                        console.log(data);
                     }
-                    return Promise.resolve("ok");
-                });
-        }).then(result => {
-            console.log('Transaction success!');
-            response.status(200).send({'data': {'text':"Hello from Firebase!"}});
-        }).catch(err => {
-            console.log('Transaction failure:', err);
-            response.status(500).send();
-        });
-    });
-});
-
-exports.readMessageFirestore = functions.https.onRequest((request, response) => {
-    return cors(request, response, () => {
-        var requesterId = request.body.data.id;
-        var encounterId = request.body.data.encounterId;
-        console.log("ReadMessage EncounterID: " + encounterId + " RequestionId: " + requesterId);
-        let ref = db.collection('encounters').doc(encounterId).collection('webrtc_signal_queue').doc(requesterId.toString());
-        db.runTransaction(t => {
-            t.get(ref).then(doc => {
-                console.log("Get success");
-                if (!doc.exists) {
-                    console.log("Doc doesn't exist");
-                    Promise.resolve(null);
-                }
-                data = doc.data();
-                console.log("Loaded: " + JSON.stringify(data, null, 2));
-                if (data == null || !data.queue || data.queue.length == 0) {
-                    return Promise.resolve(null);
-                }
-                console.log("Old Queue Length " + data.queue.length);
-                console.log("RequesterId: " + requesterId);
-                var returnVal = null;
-                for(var i = 0; i < data.queue.length; ++i) {
-                    if (data.queue[i].id == requesterId) {
-                        console.log("Skipping my own ID " + requesterId );
-                        continue;
-                    } else {
-                        console.log("Removing " + JSON.stringify(data.queue[i], null, 2));
-                        returnVal = data.queue[i];
-                        data.queue.splice(i, 1);
-                    }
-                }
-                console.log("New Queue Length " + data.queue.length);
-                t.set(ref, data);
-                return Promise.resolve(returnVal);
             });
-        }).then(result => {
-            if (result == null) {
-                console.log("Return null");
-                response.status(200).send(null);
-            } else {
-                console.log("Returning: " + JSON.stringify(result, null, 2));
-                response.status(200).send({'data': result});
-            }
-        }).catch(err => {
-            console.log("Error " + JSON.stringify(err, null, 2));
-            response.status(500).send();
-        });
+        } catch(e) {
+            console.log(e);
+        }
     });
 });
