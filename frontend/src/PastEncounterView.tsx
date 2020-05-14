48c3b270ff42c0e07ae112ded898dfa2cf1a65f3
rshellikeri <rshellikeri@vmware.com>
2020-05-14 02:00:31 -0700

    Use Card to show transcript

--- frontend/src/PastEncounterView.tsx
+++ frontend/src/PastEncounterView.tsx
@@ -1,14 +1,44 @@
 import React from "react";
 import { Encounter } from "./encounter";
 import * as firebase from "firebase/app";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { ClickAwayListener, CardActions } from "@material-ui/core";
+import { Portal } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 interface PastEncounterViewProps {
     user: firebase.User,
     encounterToView: Encounter
 }
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 export default function PastEncounterView({user, encounterToView}: PastEncounterViewProps) {
     const [transcript, setTranscript] = React.useState<String>();
+    const [open, setOpen] = React.useState(true);
+    const classes = useStyles();
+
+    const handleClickAway = () => {
+      setOpen(false);
+    };
 
     React.useEffect(() => {
         console.log("Getting transcript");
@@ -23,8 +53,24 @@ export default function PastEncounterView({user, encounterToView}: PastEncounter
     }, [user, encounterToView, setTranscript]);
 
     return (
-    <>
-    {transcript}
-    </>
+        <ClickAwayListener onClickAway={handleClickAway}>>
+            {open? (
+                <Portal>
+                    <Card className={classes.root} variant="outlined">
+                    <CardContent>
+                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        Visit Transcript
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                        {transcript}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" onClick={handleClickAway}>Close</Button>
+                    </CardActions>
+                    </Card>
+                </Portal>
+            ): null}
+        </ClickAwayListener>
     );
 };
